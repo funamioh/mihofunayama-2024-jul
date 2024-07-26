0ff31bbad11eaa35f2492436792d47d453bc81e6
Miho Funayama <funamiho1011@gmail.com>
2024-07-26 21:47:04 +0900

    added typing effect

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,36 @@
+document.addEventListener("DOMContentLoaded", typeWriter);
+
 let i = 0;
 const text = "Miho Funayama";
-const speed = 50;
+const space = "&nbsp;"; // to insert empty space
+const speed = 300;
+let isDeleting = false;
 
 function typeWriter() {
+  const nameElement = document.getElementById("nameType");
+
+if (!isDeleting) {
+  // adding characters
   if (i < text.length) {
-    document.getElementById("nameType").innerHTML += text.charAt(i);
+    nameElement.innerHTML += text.charAt(i);
     i++;
-    setTimeout(() => {
-      typeWriter
-    }, speed);
+    setTimeout(typeWriter, speed);
+  } else {
+    isDeleting = true;
+    setTimeout(typeWriter, speed);
   }
+} else {
+  // deleting characters
+  if (i > 1) {
+    // text[0]からtext[i - 1]まで表示
+    nameElement.innerHTML = text.substring(0, i - 1);
+    i--;
+    setTimeout(typeWriter, speed);
+  } else {
+    nameElement.innerHTML = space;
+    i--;
+    isDeleting = false;
+    setTimeout(typeWriter, speed);
+  }
+}
 }
